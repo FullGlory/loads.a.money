d2f11f0e662ac9cfdfa27544d5e6e8a0e91807cb
Paul Marshall <paul.marshall@wavecrestsoftware.co.uk>
2013-06-11 22:20:09 +0100

    Optimised saving in repo

--- SpreadBet.Common/Components/StockDataProvider.cs
+++ SpreadBet.Common/Components/StockDataProvider.cs
@@ -30,9 +30,12 @@ namespace SpreadBet.Common.Components
 		{
             foreach(var sp in stockPrice)
             {
-                _repository.SaveOrUpdate<Period>(sp.Period);
-                _repository.SaveOrUpdate<Stock>(sp.Stock);
-                _repository.SaveOrUpdate<Price>(sp.Price);
+                // JC - you don't need these 3 updates as the StockPrice is the "aggregate root" 
+                // and so will save all the associated entities
+                // TODO - should this be done in a tx as a batch is being updated...maybe the Unit Of Work pattern here??
+                //_repository.SaveOrUpdate<Period>(sp.Period);
+                //_repository.SaveOrUpdate<Stock>(sp.Stock);
+                //_repository.SaveOrUpdate<Price>(sp.Price);
                 _repository.SaveOrUpdate<StockPrice>(sp);
             }
 		}
