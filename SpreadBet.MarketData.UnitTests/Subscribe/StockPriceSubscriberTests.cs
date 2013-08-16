c84092d11254ad3e23b96c7589580116c725763c
Paul Marshall <paul.marshall@wavecrestsoftware.co.uk>
2013-08-16 13:46:24 +0100

    Added dead letter q to price subscriber

--- SpreadBet.MarketData.UnitTests/Subscribe/StockPriceSubscriberTests.cs
+++ SpreadBet.MarketData.UnitTests/Subscribe/StockPriceSubscriberTests.cs
@@ -4,6 +4,7 @@ using SpreadBet.Common.Interfaces;
 using SpreadBet.Domain;
 using SpreadBet.Infrastructure.Messaging;
 using SpreadBet.MarketData.Subscribe;
+using System;
 
 namespace SpreadBet.MarketData.UnitTests.Subscribe
 {
@@ -11,6 +12,7 @@ namespace SpreadBet.MarketData.UnitTests.Subscribe
     public class StockPriceSubscriberTests
     {
         private Mock<IMessageReceiver> _mockInputChannel;
+        private Mock<IMessageSender> _mockDeadLetterChannel;
         private Mock<IStockDataProvider> _mockRepository;
         private StockPriceSubscriber _subscriber;
 
@@ -18,8 +20,9 @@ namespace SpreadBet.MarketData.UnitTests.Subscribe
         public void SetUp()
         {
             _mockInputChannel = new Mock<IMessageReceiver>();
+            _mockDeadLetterChannel = new Mock<IMessageSender>();
             _mockRepository = new Mock<IStockDataProvider>();
-            _subscriber = new StockPriceSubscriber(_mockInputChannel.Object, _mockRepository.Object);
+            _subscriber = new StockPriceSubscriber(_mockInputChannel.Object, _mockRepository.Object, _mockDeadLetterChannel.Object);
         }
 
         [Test]
@@ -47,6 +50,23 @@ namespace SpreadBet.MarketData.UnitTests.Subscribe
             // Assert
             // TODO - work out how to unpack message into stockprice
             _mockRepository.Verify(x => x.AddStockPrice(It.IsAny<StockPrice>()), Times.Once());
+        }
+
+        [Test]
+        public void MovesStockPriceToDeadLetterQueueWhenAttemptToStoreUpdateFails()
+        {
+          // Arrange
+          _subscriber.Start();
+          var seedMsg = new Message();
+
+          _mockRepository.Setup(x => x.AddStockPrice(It.IsAny<StockPrice>())).Throws<Exception>();
+
+          // Act
+          _mockInputChannel.Raise(x => x.MessageReceived += null, new MessageReceivedEventArgs(seedMsg));
+
+          // Assert
+          // TODO - work out how to unpack message into stockprice
+          _mockDeadLetterChannel.Verify(x => x.Send(seedMsg), Times.Once());
         }
     }
 }
--- SpreadBet.MarketData/Subscribe/StockPriceSubscriber.cs
+++ SpreadBet.MarketData/Subscribe/StockPriceSubscriber.cs
@@ -1,34 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SpreadBet.Common.Interfaces;
+﻿using SpreadBet.Common.Interfaces;
 using SpreadBet.Domain;
 using SpreadBet.Infrastructure.Messaging;
+using System;
 
 namespace SpreadBet.MarketData.Subscribe
 {
     public class StockPriceSubscriber
     {
-        private readonly IMessageReceiver _channel;
+        private readonly IMessageReceiver _dataFeedChannel;
         private readonly IStockDataProvider _stockDataProvider;
+        private readonly IMessageSender _deadLetterChannel;
 
-        public StockPriceSubscriber(IMessageReceiver channel, IStockDataProvider stockDataProvider)
+        public StockPriceSubscriber(IMessageReceiver dataFeedChannel, IStockDataProvider stockDataProvider, IMessageSender deadLetterChannel)
         {
-            this._channel = channel;
-            this._stockDataProvider = stockDataProvider;
+          this._dataFeedChannel = dataFeedChannel;
+          this._stockDataProvider = stockDataProvider;
+          this._deadLetterChannel = deadLetterChannel;
 
-            this._channel.MessageReceived += MessageReceived;
+          this._dataFeedChannel.MessageReceived += MessageReceived;
         }
 
         public void Start()
         {
-            this._channel.Start();
+            this._dataFeedChannel.Start();
         }
 
         private void MessageReceived(object sender, MessageReceivedEventArgs e)
         {
+          var msg = e.Message;
+
+          try
+          {
             _stockDataProvider.AddStockPrice(new StockPrice());
+          }
+          catch (Exception ex)
+          {
+            _deadLetterChannel.Send(msg);
+          }
         }
     }
 }
